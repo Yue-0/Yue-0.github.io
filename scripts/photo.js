4515d84a7f59493b5031dd9e970a048a8d7e4838
Yue-0 <582142420@qq.com>
2025-01-03 02:27:56 +0800

    Using CSS and JavaScript

---
+++ scripts/photo.js
@@ -0,0 +1,41 @@
+/* author: YueLin */
+
+const photos = [
+    {year: "2024", src: "img/2024.jpg"},
+    {year: "2023", src: "img/2023.jpg"},
+    {year: "2022", src: "img/2022.jpg"},
+    {year: "2021", src: "img/2021.jpg"},
+    {year: "2020", src: "img/2020.jpg"}
+];
+
+const photo = document.getElementById("photo");
+const container = document.getElementById("photos");
+
+photos.forEach(photo => {
+    const img = document.createElement("img");
+    img.src = photo.src;
+    img.alt = photo.year;
+
+    const year = document.createElement("span");
+    year.textContent = photo.year;
+
+    const div = document.createElement("div");
+    div.className = "photo";
+    div.appendChild(img);
+    div.appendChild(year);
+    container.appendChild(div);
+});
+
+photo.addEventListener("mouseover", () => {
+    document.querySelectorAll(".photo").forEach(item => {
+        item.style.opacity = "1";
+        item.style.height = "160px";
+    });
+});
+
+photo.addEventListener("mouseout", () => {
+    document.querySelectorAll(".photo").forEach(item => {
+        item.style.opacity = "0";
+        item.style.height = "0";
+    });
+});
