2560ee53ea6d7e72ce9c05b0a4b47734ef9ad395
Yue-0 <582142420@qq.com>
2025-01-11 02:49:14 +0800

    Add images

--- scripts/language.js
+++ scripts/language.js
@@ -24,26 +24,34 @@ var text = {
         
         "projects": "Projects",
 
+        "nsfc_code": "Code",
+        "nsfc_video": "Video",
         "nsfc": "NSFC Major Project",
         "nsfc_authors": "<b>Yue Lin</b>, Peng Zhang, Hangyou Yu, Yang Liu, Dong Wang, Huchuan Lu",
         "nsfc_title": "Learning Active Perception and Adversarial Game Modeling of Single-agent",
         "nsfc_details": "We use the RoboMaster2020 standard AI robot to conduct 1v1 fully automatic confrontation games. The robot can actively perceive the environment and enemy target, and establish intelligent strategies to automatically conduct confrontation games.",
 
-        "tsinghua": "NSFC Project, Tsinghua University Exchange Project",
+        "tsinghua_code": "Code",
+        "tsinghua_video": "Video",
+        "tsinghua": "Tsinghua University Exchange & NSFC Major Project",
         "tsinghua_authors": "Di Wu, Zijie Zhao, <b>Yue Lin</b>, Wenhao Zhou, Huchuan Lu, You He",
         "tsinghua_title": "Methods and Applications of Multi-agent Confrontation Games in Open Environments",
         "tsinghua_details": "We developed multi-robot collaborative capture algorithms in open environments, including environmental perception, target recognition, motion planning, and strategy learning, so that the robot cluster can quickly and accurately capture an intelligent target in unknown environments.",
 
+        "201code": "Code",
+        "201video": "Video",
         "201": "201 Institute Cooperation Project",
         "201authors": "<b>Yue Lin</b>, Ziqi guan, Ben Kang, Xin Chen, Dong Wang, Huchuan Lu",
         "201title": "Research on Lightweight Object Tracking Method Based on Transformers",
-        "201details": "We deployed high-precision, lightweight Transformer-based object tracking models on JetSon Orin NX, which show robust performance for small and frequently occluded objects, and can run in real time on edge devices.",
+        "201details": "We developed a high-precision, lightweight single object tracking model based on Transformer for the 201 Institute. The model shows robust performance for small targets and frequently occluded targets, and can run in real time at 110 FPS on edge devices such as JetSon Orin NX. The model has been applied to the gimbal for automatic tracking of small targets at long distances.",
 
         "competitions": "Competition",
 
+        "icra22web": "Website",
+        "icra22video": "Video",
         "icra22": "4th in the World",
         "icra22authors": "Shiyao Li, <b>Yue Lin</b>, Zuyao You, Jiansong Pei, Zeyun Wang, Feilong Wang",
-        "icra22details": "We developed algorithms in a simulation environment that enable the robot to autonomously find and grab the corresponding ores, and autonomously navigate to transport them to the designated location. The algorithms developed in the simulation environment can be directly deployed on the real robot."
+        "icra22details": "We developed lightweight visual recognition and navigation algorithms in a simulation environment, which enabled the robot to autonomously search and grab the corresponding ores, and transport them to the designated location through autonomously navigation. The algorithms developed in the simulation environment can be directly deployed on the real robot."
     },
 
     "zh": {
@@ -69,26 +77,34 @@ var text = {
         
         "projects": "项目",
 
-        "nsfc": "国家自然科学基金委员会重大项目",
+        "nsfc_code": "代码",
+        "nsfc_video": "视频",
+        "nsfc": "国家自然科学基金重大项目",
         "nsfc_title": "单智能体主动感知和对抗博弈建模学习",
         "nsfc_authors": "<b>林跃</b>，张鹏，于沆佑，刘洋，王栋，卢湖川",
         "nsfc_details": "使用 RoboMaster2020 标准 AI 机器人进行 1v1 全自动对抗博弈。机器人能够主动感知环境和敌方目标，并建立智能策略自动进行对抗博弈。",
 
-        "tsinghua": "国家自然科学基金委员会重大项目，清华大学交流项目",
+        "tsinghua_code": "代码",
+        "tsinghua_video": "视频",
+        "tsinghua": "清华大学交流项目，国家自然科学基金重大项目",
         "tsinghua_authors": "吴迪，赵子杰，<b>林跃</b>，周文浩，卢湖川，何友",
         "tsinghua_title": "开放环境多智能体群智对抗博弈理论方法及应用",
         "tsinghua_details": "开发开放环境下多机器人协同围捕算法，包含环境感知、目标识别、运动规划、策略学习等，使得机器人集群能够快速、精准地在未知环境下对智能目标实行围捕。",
 
+        "201code": "代码",
+        "201video": "视频",
         "201": "中国北方车辆研究所合作项目",
         "201authors": "<b>林跃</b>，关子奇，康奔，陈鑫，王栋，卢湖川",
         "201title": "基于 Transformer 的轻量化目标跟踪方法研究",
-        "201details": "在 JetSon Orin NX 上部署基于 Transformer 的高精度、轻量化的目标跟踪模型，模型对小型目标和频繁被遮挡的目标表现出鲁棒的性能，且在边缘计算设备上实时运行。",
+        "201details": "为中国北方车辆研究所开发基于 Transformer 的高精度、轻量化的单目标跟踪模型，模型对小型目标和频繁被遮挡的目标表现出鲁棒的性能，且可以在 JetSon Orin NX 等边缘计算设备上以 110 FPS 的帧率实时运行。该模型被应用在用于自动跟踪远距离小型目标的云台上。",
 
         "competitions": "竞赛",
 
+        "icra22web": "网页",
+        "icra22video": "视频",
         "icra22": "全球第四名",
         "icra22authors": "李世垚，<b>林跃</b>，尤祖尧，裴健淞，王泽允，王飞龙",
-        "icra22details": "在仿真环境中开发算法使得机器人能够自主寻找和抓取相应的矿石，并自主导航将其运送到指定的位置。在仿真环境中开发的算法可以直接部署到真实机器人上。"
+        "icra22details": "在仿真环境中开发轻量化视觉识别算法和导航算法，使得机器人能够在环境中自主寻找和抓取相应的矿石，并通过自主导航将其运送到指定的位置。在仿真环境中开发的算法可以直接部署到真实的机器人上。"
     }
 }
 
@@ -106,6 +122,8 @@ function update()
     });
     document.querySelectorAll(".details").forEach((details) => {
         setTimeout(() => {
+            if(details.classList.contains("hidden"))
+                details.classList.remove("hidden");
             details.style.height = details.scrollHeight + "px";
         }, 350);
     });
